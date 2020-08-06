c070aa23328e57899fc92f09e08fb4f51a47156c
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-08-06 19:57:22 -0300

    Stack Navigation

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
 import landingImage from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
+  const navigation = useNavigation();
+
+  function handleNavigateToGiveClassesPage() {
+    navigation.navigate('GiveClasses');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImage} style={styles.banner} />
@@ -18,20 +27,20 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
-          <Image source={studyIcon}/>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
-          <Image source={giveClassesIcon}/>
+        <RectButton onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
+          <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}>Dar aulas</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections}>
         Total de 200 conexões já realizadas {' '}
-        <Image source={heartIcon}/>
+        <Image source={heartIcon} />
       </Text>
     </View>
   );
