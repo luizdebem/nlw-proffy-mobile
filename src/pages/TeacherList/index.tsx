a6816c89d45cf02d07503621c7c07ee0f935075c
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-08-07 20:03:43 -0300

    Teacher Item component

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 export default function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   )
 }
